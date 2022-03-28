0ef2c267f36b4ca36c76462855e3f8f3c7747186
msanders13 <97141929+msanders13@users.noreply.github.com>
2022-03-28 15:19:58 -0500

    message

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -2,7 +2,7 @@ import "./footer.css"; // import your CSS
 
 const Footer = () => {
     return (<div className='footer'>
-                <footer>&copy; <em id="date"></em>CheeseStore 2022</footer>
+                <footer>&copy; <em id="date"></em>The CheeseStore 2022</footer>
             </div>);
 };
 
